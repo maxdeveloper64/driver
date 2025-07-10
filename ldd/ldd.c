5099a45e594c6f4fcfc484c4ea9c3bb0407fd5c2
tizio <myemail@mail.com>
2025-07-10 12:02:44 +0200

    first code in ldd.c

---
+++ ldd/ldd.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Tizio");
+MODULE_DESCRIPTION("Linuz Device Driver");
+
+static int ldd_module_init(void) {
+    printk("Inited ldd driver\n");
+    return 0;
+}
+
+static void ldd_module_exit(void) {
+    printk("Exit ldd mdriver\n");
+}
+
+module_init(ldd_module_init);
+module_exit(ldd_module_exit);
